56c17df1d671c7d8f0cfc2e9d2418714bd8027ed
Suarmin Rais <suarmin.rais@gmail.com>
2021-04-14 21:33:36 +0800

    changed logo and menu list

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import './navbar.styles.css'
+import logo from './logo.png'
 
 const Navbar = () => {
   const [sticky, setSticky] = useState(false);
@@ -23,15 +24,19 @@ const Navbar = () => {
       <div className="container">
         <div className="logo">
           <a href="/">
-            <img src="https://bibit.id/img/logoBibitFix.svg" alt="" />
+            <img src={logo} alt="inafitgo" />
           </a>
         </div>
         <div className="menus">
           <nav>
             <ul>
-              <li><a href="/">As</a></li>
-              <li><a href="/">As</a></li>
-              <li><a href="/">As</a></li>
+              <li><a href="/">Home</a></li>
+              <li><a href="#tentang">About</a></li>
+              <li><a href="#feature">Feature</a></li>
+              <li><a href="#client">Client</a></li>
+              <li><a href="/event">Event</a></li>
+              <li><a href="/blog">Blog</a></li>
+              <li><a href="/contact">Contact</a></li>
             </ul>
           </nav>
         </div>
